923e8c063b6991541e8864504913fce5a0576af2
Jean Chassoul <chassoul@gmail.com>
2015-09-22 14:47:51 -0600

    chevelle for the stuff

--- static/js/seed/fun.router.js
+++ static/js/seed/fun.router.js
@@ -80,11 +80,10 @@ fun.Router = Backbone.Router.extend({
     },
 
     landing: function(){
-
         fun.utils.hideAll();
-        fun.instances.navbar.render();
-        console.log('after landing');
-        fun.instances.landing.render();
+        //fun.instances.navbar.render();
+        //console.log('after landing');
+        //fun.instances.landing.render();
         fun.instances.footer.render();
     },
 
